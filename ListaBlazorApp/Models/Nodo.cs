613eb950c2a8ccf4d27dd67b0d42ad74e276688c
JOSE LUIS XILOJ PEREZ <jxilojp2@miumg.edu.gt>
2024-03-18 22:33:16 -0600

    Add project files.

---
+++ ListaBlazorApp/Models/Nodo.cs
@@ -0,0 +1,32 @@
+﻿namespace ListaBlazorApp.Models
+{
+    public class Nodo
+    {
+        public object Informacion { get; set; }
+
+        public Nodo Referencia { get; set; }
+
+        public Nodo()
+        {
+            Informacion = null;
+            Referencia = null;
+        }
+
+        public Nodo(object informacion)
+        {
+            Informacion = informacion;
+            Referencia = null;
+        }
+
+        public Nodo(object informacion, Nodo referencia)
+        {
+            Informacion = informacion;
+            Referencia = referencia;
+        }
+
+        public override string ToString()
+        {
+            return $"{Informacion}";
+        }
+    }
+}
---
+++ ListaBlazorApp/Services/ListaEnlazadaSimple.cs
@@ -0,0 +1,66 @@
+﻿using ListaBlazorApp.Models;
+using System.Collections;
+
+namespace ListaBlazorApp.Services
+{
+    public class ListaEnlazadaSimple : IEnumerable
+    {
+        public Nodo PrimerNodo { get; set; }
+        public Nodo UltimoNodo { get; set; }
+
+        public ListaEnlazadaSimple()
+        {
+            PrimerNodo = null;
+            UltimoNodo = null;
+        }
+
+        public bool ListaVacia()
+        {
+            return PrimerNodo == null;
+        }
+
+        public string AgregarNodoAlFinal(Nodo nuevoNodo)
+        {
+            if (ListaVacia())
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                UltimoNodo.Referencia = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+
+            return "Se ha agregado el nodo al final";
+        }        
+        
+        public string AgregarNodoAlInicio(Nodo nuevoNodo)
+        {
+            if (ListaVacia())
+            {
+                PrimerNodo = nuevoNodo;
+                UltimoNodo = nuevoNodo;
+            }
+            else
+            {
+                nuevoNodo.Referencia = PrimerNodo;
+                PrimerNodo = nuevoNodo;
+
+            }
+
+            return "Se ha agregado el nodo al inicio";
+        }
+
+        public IEnumerator GetEnumerator()
+        {
+            Nodo nodoAuxiliar = PrimerNodo;
+
+            while (nodoAuxiliar != null)
+            {
+                yield return nodoAuxiliar;
+                nodoAuxiliar = nodoAuxiliar.Referencia;
+            }
+        }
+    }
+}
